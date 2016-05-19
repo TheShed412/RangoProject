4c7e1e9f55bf4b9f6e952912a0b9cffd0edb7af4
Tyler <shed412@gmail.com>
2016-05-19 14:30:07 -0400

    Finished 8

--- code/tango_with_django_project/rango/urls.py
+++ code/tango_with_django_project/rango/urls.py
@@ -3,4 +3,5 @@ from rango import views
 
 urlpatterns = patterns('',url(r'^$', views.index, name = 'Index'),
                           url(r'^about/', views.about, name = 'About'),
+                          url(r'^add_category/$', views.add_category, name='add_category'),
                           url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),)
--- code/tango_with_django_project/rango/views.py
+++ code/tango_with_django_project/rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 # Create your views here.
 def index(request):
@@ -16,16 +17,30 @@ def about(request):
 
 def category(request, category_name_slug):
     context_dict = {}
-    
+
     try:
         category = Category.objects.get(slug=category_name_slug)
         context_dict['category_name'] = category.name
         pages = Page.objects.filter(category=category)
-        
+
         context_dict['pages'] = pages
         context_dict['category'] = category
-        
+
     except Category.DoesNotExist:
         pass
-    
+
     return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print form.errors
+    else:
+        form = CategoryForm()
+
+    return render(request, 'rango/add_category.html', {'form':form})
